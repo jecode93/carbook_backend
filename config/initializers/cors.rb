2d5e948b79704728c3ee8cd3c6d0a33c9ad5ab3a
jecode93 <jeanemmanuelcadet@gmail.com>
2023-12-14 11:40:56 -0500

    Update cors.rb file.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
-  config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*' # Add the origin of your frontend application
-      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*' # Add the origin of your frontend application
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
+end
