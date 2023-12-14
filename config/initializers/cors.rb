77fd8f9f133525f6d039a5d6f42780d83fb77bc5
jecode93 <jeanemmanuelcadet@gmail.com>
2023-12-14 11:44:43 -0500

    Update origin link in cors.rb file.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # Add the origin of your frontend application
+    origins 'https://final-capstone-frontend-3l0g.onrender.com' # Add the origin of your frontend application
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
